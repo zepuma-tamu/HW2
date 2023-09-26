0ddf8ad98c02d51020a4cda678db11cac0ab8223
Eric <108982075+zepuma@users.noreply.github.com>
2023-09-26 15:22:37 -0500

    the last

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@
 more_movies = [
   {:title => 'Cloud Atlas', :rating => 'R',
     :release_date => '26-Oct-1988'},
+  {:title => 'My Neighbor Totoro', :rating => 'G',
+    :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13',
+    :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R',
+    :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R',
+    :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'}
 ]
 
 more_movies.each do |movie|
