79407393a75fbaa9236d9aa8c1ebb05cb439b264
Eric <108982075+zepuma@users.noreply.github.com>
2023-09-26 15:16:52 -0500

    last

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,7 @@
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  {:title => 'Cloud Atlas', :rating => 'R',
+    :release_date => '26-Oct-1988'},
 ]
 
 more_movies.each do |movie|
